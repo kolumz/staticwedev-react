ca29d20c23969c46bbc52be6036d85e30c0df366
Mostafa Shaban <mostafashabanelbanna@gmail.com>
2022-03-11 14:45:52 +0200

    getArticles

--- src/views/home/Feed.js
+++ src/views/home/Feed.js
@@ -8,12 +8,11 @@ const Feed = () => {
   const [articles, setArticles] = useState();
 
   const api =
-    "https://newsapi.org/v2/everything?q=Apple&from=2022-03-11&sortBy=popularity&apiKey=74d91dc51acf41758e5cab786c8d1d77";
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=74d91dc51acf41758e5cab786c8d1d77";
   const getArticles = () => {
     fetch(api)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json.articles);
         setArticles(json.articles);
       });
   };
@@ -24,14 +23,15 @@ const Feed = () => {
 
   return (
     <Box sx={{ width: "100%", minHeight: 829 }}>
-      <Masonry columns={{ lg: 4, md: 3, sm: 2, xs: 1 }} spacing={2}>
-        {articles &&
-          articles.map((item, index) => (
+      {articles && (
+        <Masonry columns={{ lg: 4, md: 3, sm: 2, xs: 1 }} spacing={2}>
+          {articles.map((item, index) => (
             <div key={index}>
               <NewsletItem article={item} />
             </div>
           ))}
-      </Masonry>
+        </Masonry>
+      )}
     </Box>
   );
 };
